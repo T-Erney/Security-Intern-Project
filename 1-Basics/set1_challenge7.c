5698454a002a541b5fce0f5a04d9465a90ea1aa8
Tristan Erney <terney@live.esu.edu>
2021-09-22 21:37:00 -0400

    challenge 7 complete

---
+++ 1-Basics/set1_challenge7.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/aes.h>
+
+#include "conversions.h"
+#include "xor.h"
+#include "memory_output.h"
+
+
+
+int main() {
+
+  char* b64 = NULL;
+  char* key = "YELLOW SUBMARINE";
+  {
+    FILE* file = fopen("7.txt", "r");
+
+    fseek(file, 0, SEEK_END);
+    size_t size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    b64 = malloc(sizeof(char) * size + 1);
+    b64[size] = 0;
+
+    fread(b64, 1, size, file);
+    fclose(file);
+  }
+
+  byte_string* c_bytes = base64_to_bytes(b64);
+  byte_string* k_bytes = string_to_bytes(key);
+  byte_string* p_bytes = bytes_init(c_bytes->size);
+
+  // AES decrypt key
+  AES_KEY aes_decrypt_key;
+  AES_set_decrypt_key(k_bytes->data, 128, &aes_decrypt_key);
+
+  size_t i = 0;
+  while (i < c_bytes->size) {
+    AES_ecb_encrypt(c_bytes->data + i, p_bytes->data + i, &aes_decrypt_key, AES_DECRYPT);
+    p_bytes->size += 16;
+    i += 16;
+  }
+
+  printf("%s\n", bytes_to_string(p_bytes));
+  
+  return 0;
+}
