1564665148d1ce8a4b4838f6131745d8299f66f2
Tristan Erney <terney@live.esu.edu>
2021-09-22 20:28:05 -0400

    updating hierarchy

--- 1-Basics/set1_challenge6.c
+++ 1-Basics/set1_challenge6.c
@@ -8,17 +8,25 @@
 #include "hamming_dist.h"
 
 static char letter_freq_order[] = {
-  ' ', 'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 
-  'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 
-  'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'
+                                   ' ', 'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 
+                                   'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 
+                                   'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'
 };
 
-static float letter_freq_value[] = {
-  13.000, 12.702, 9.056, 8.167, 7.507, 6.966, 6.749, 6.327, 6.094,
-   5.987,  4.253, 4.025, 2.782, 2.758, 2.406, 2.360, 2.228, 2.015,
-   1.974,  1.929, 1.492, 0.978, 0.772, 0.153, 0.150, 0.095, 0.074
+static double letter_freq_value[] = {
+                                    13.000, 12.702, 9.056, 8.167, 7.507, 6.966, 6.749, 6.327, 6.094,
+                                    5.987,  4.253, 4.025, 2.782, 2.758, 2.406, 2.360, 2.228, 2.015,
+                                    1.974,  1.929, 1.492, 0.978, 0.772, 0.153, 0.150, 0.095, 0.074
 };
 
+double get_letter_freq(char c) {
+  double val = 0.0;
+  for (size_t i = 0; i < 27; i += 1) {
+    if (c == letter_freq_order[i]) val = letter_freq_value[i];
+  }
+
+  return val;
+}
 
 // ---
 
@@ -32,198 +40,101 @@ char* substr(char* str, size_t a, size_t b) {
 
 // ---
 
+double get_score(byte_string* bytes) {
+  double score = 0.0;
+  for (size_t i = 0; i < bytes->size; i += 1) {
+    score += get_letter_freq(bytes->data[i]);
+  }
 
+  return score / bytes->size;
+}
 
 int main() {
-
-  /*
-  { // TESTING HAMMING DISTANCE FUNCTION
-    char* test1 = "this is a test";
-    char* test2 = "wokka wokka!!!";
-
-    int h_dist = hamming_dist(test1, test2);
-    printf("[TEST] h_dist = %d\n\n", h_dist);
-  }
-  */
-
+  char* b64 = NULL;
   {
-    // get the contents of 6.txt
-    char* buffer = NULL;
-    {
-      FILE* file = fopen("6.txt", "r");
-      size_t size = 0;
+    FILE* file = fopen("6.txt", "r");
+    size_t size = 0;
 
-      fseek(file, 0, SEEK_END);
-      size = ftell(file);
-      fseek(file, 0, SEEK_SET);
+    fseek(file, 0, SEEK_END);
+    size = ftell(file);
+    fseek(file, 0, SEEK_SET);
 
-      buffer = malloc(sizeof(char) * size + 1);
-      fread(buffer, 1, size, file);
-      buffer[size] = 0;
+    b64 = malloc(sizeof(char) * size);
+    fread(b64, 1, size - 1, file);
+    b64[size - 1] = 0;
 
-      fclose(file);
-    }
-
-    // un-base64 the file
-    byte_string* c_bytes = base64_to_bytes(buffer);
-    char* hex = bytes_to_hex(c_bytes);
-    bytes_free(c_bytes); // free since we don't need anymore
-
-    float smallest_normalized_dist = 0x0000ffff;
-    int   smallest_dist = 0;
-    int   dists[38] = { 0 };
-
-    for (size_t k_size = 2; k_size <= 40; k_size += 1) {
-      char* sec1 = substr(hex, 0, k_size);
-      char* sec2 = substr(hex, k_size, k_size);
-
-      int h_dist = hamming_dist(sec1, sec2);
-      float normalized_dist = (float)h_dist / (float)k_size;
-
-      /*
-      if (normalized_dist < smallest_normalized_dist) {
-        smallest_dist = h_dist;
-        smallest_normalized_dist = normalized_dist;
-      }
-      */
-
-      dists[40 - k_size] = h_dist;
-      printf("%d ", h_dist);
-
-      free(sec1);
-      free(sec2);
-    }
-    printf("\n");
-
-    // orginize from least to greatest
-    int smallest_dists[3] = { 0 };
-    for (size_t i = 0; i < 3; i += 1) {
-      int s = 999;
-      int s_val = 999999;
-      for (size_t j = i; j < 38; j += 1) {
-        if (s_val >= dists[j]) { s = j; s_val = dists[j]; }
-      }
-
-      int tmp = dists[i];
-      dists[i] = dists[s];
-      dists[s] = tmp;
-      smallest_dists[i] = dists[i];
-    }
-
-    printf("smallest distances            :: %d, %d, %d\n", smallest_dists[0], smallest_dists[1], smallest_dists[2]);
-
-    /*
-    printf("Smallest Normalized Distance  :: %f\n", smallest_normalized_dist);
-    printf("Smallest Hamming Distance     :: %d\n", smallest_dist);
-    */
-
-    for (int k = 0; k < 3; k += 1) {
-
-      smallest_dist = smallest_dists[k];
-    
-    size_t chunk_count = strlen(hex) / smallest_dist;
-    char** chunks = malloc(sizeof(char*) * chunk_count);
-    {
-      size_t i = 0;
-      for (size_t j = 0; j < chunk_count; j += 1) {
-        chunks[j] = malloc(sizeof(char) * smallest_dist);
-        memcpy(chunks[j], hex + i, smallest_dist);
-        i += smallest_dist;
-      }
-    }
-
-    char** transposed_chunks = malloc(sizeof(char*) * smallest_dist);
-    {
-      size_t i = 0;
-      for (size_t j = 0; j < smallest_dist; j += 1) {
-        transposed_chunks[j] = malloc(sizeof(char*) * chunk_count + 1);
-        transposed_chunks[j][chunk_count] = 0;
-        for (size_t k = 0; k < chunk_count; k += 1) {
-          transposed_chunks[j][k] = chunks[k][j];
-        }
-      }
-    }
-
-    // single xor on all chunks
-    char* keys = malloc(sizeof(char) * smallest_dist);
-    memset(keys, 0, smallest_dist);
-    {
-      for (size_t i = 0; i < smallest_dist; i += 1) {
-        char* best_text = NULL;
-        float best_score = 0.0;
-        unsigned char best_key = 0;
-        byte_string* bytes = hex_to_bytes(transposed_chunks[i]);
-
-        for (int key = 1; key < 256; key += 1) {
-          char* text = NULL;
-          text = malloc(sizeof(char) * bytes->size);
-          text[bytes->size] = 0;
-          for (size_t i = 0; i < bytes->size; i += 1) {
-            unsigned char xor = _xor(bytes->data[i], key);
-            text[i] = xor;
-          }
-
-          float score = 0.0;
-          for (size_t i = 0; i < bytes->size; i += 1) {
-            int j = 0;
-            int other = 1;
-            for (; j < 27; j += 1) {
-              if (text[i] == letter_freq_order[j]) {
-                other = 0;
-                break;
-              }
-            }
-
-            score += (other) ? 0.50 : letter_freq_value[j];
-          }
-
-          if (score > best_score) {
-            best_score = score;
-            if (best_text != NULL) free(best_text);
-            best_text = text;
-            best_key = key;
-          } else {
-            free(text);
-          }
-        }
-
-        keys[i] = best_key;
-        bytes_free(bytes);
-        free(best_text);
-      }
-    }
-
-    printf("Keys:\n\t[ ");
-    for (size_t i = 0; i < smallest_dist; i += 1) {
-      printf("%d ", keys[i]);
-    }
-    printf("]\n");
-
-    char* output = NULL;
-    {
-      byte_string* h_bytes = hex_to_bytes(hex);
-      byte_string* k_bytes = string_to_bytes(keys);
-      byte_string* o_bytes = bytes_init(h_bytes->size);
-
-      for (size_t i = 0; i < h_bytes->size; i += 1) {
-        bytes_append(o_bytes, _xor(h_bytes->data[i], k_bytes->data[i % k_bytes->size]));
-      }
-
-      output = bytes_to_string(o_bytes);
-    }
-
-    printf("\n%s\n", output);
-
-    // cleanup
-    for (size_t i = 0; i < chunk_count; i += 1) free(chunks[i]);
-    for (size_t i = 0; i < smallest_dist; i += 1) free(transposed_chunks[i]);
-    free(chunks);
-    free(transposed_chunks);
+    fclose(file);
   }
 
-    free(buffer);
+  byte_string* c_bytes = base64_to_bytes(b64);
+
+  double normal_dist = 999.0;
+  size_t K = 0;
+  for (size_t key = 2; key < 40; key += 1) {
+    double dist = 0;
+    double iter = 0;
+    for (size_t i = 0; i < c_bytes->size; i += key) {
+      byte_string* x_bytes = bytes_init(key);
+      byte_string* y_bytes = bytes_init(key);
+      memcpy(x_bytes->data, c_bytes->data + i, key);
+      memcpy(y_bytes->data, c_bytes->data + i + key, key);
+      x_bytes->size = key;
+      y_bytes->size = key;
+
+      dist += hamming_dist(x_bytes, y_bytes) / key;
+      iter += 1;
+
+      bytes_free(x_bytes);
+      bytes_free(y_bytes);
+    }
+
+    dist /= iter;
+
+    if ((normal_dist) > (dist)) {
+      normal_dist = dist;
+      K = key;
+    }
   }
 
+  byte_string** chunks = malloc(sizeof(byte_string) * K);
+  for (size_t i = 0; i < K; i += 1) {
+    chunks[i] = bytes_init(c_bytes->size / K);
+    for (size_t j = i; j < c_bytes->size; j += K) {
+      bytes_append(chunks[i], c_bytes->data[j]);
+    }
+  }
 
+  char* _key = malloc(sizeof(char) * K + 1);
+  _key[K] = 0;
+
+  for (size_t i = 0; i < K; i += 1) {
+    size_t best_score = 0;
+    size_t best_key   = 0;
+    byte_string* bytes = chunks[i];
+
+    for (unsigned char j = 0; j < 128; j += 1) {
+      byte_string* t_bytes = bytes_init(bytes->size);
+
+      for (size_t k = 0; k < bytes->size; k += 1) {
+        unsigned char xor = _xor(bytes->data[k], j);
+        bytes_append(t_bytes, xor);
+      }
+
+      double score = get_score(t_bytes);
+
+      if (score > best_score) {
+        best_score = score;
+        best_key = j;
+        bytes_free(t_bytes);
+      }
+    }
+
+    _key[i] = best_key;
+  }
+
+  printf("_key = %s\n", _key);
+
+  free(_key);
+  bytes_free(c_bytes);
   return 0;
 }
