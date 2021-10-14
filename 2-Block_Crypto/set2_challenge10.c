5a701841710daa3bf0d84463b54cd0041f70450f
Tristan Erney <terney@live.esu.edu>
2021-10-14 11:02:10 -0400

    Set 2 Challenge 10 - Update

--- 2-Block_Crypto/set2_challenge10.c
+++ 2-Block_Crypto/set2_challenge10.c
@@ -10,7 +10,7 @@
 #include "../headers/assert.h"
 
 byte_string* bytes_xor(byte_string* x_bytes, byte_string* y_bytes) {
-  if (x_bytes->size != y_bytes->size) return NULL;
+  assert(x_bytes->size == y_bytes->size);
   byte_string* bytes = bytes_init(x_bytes->size);
 
   for (size_t i = 0; i < x_bytes->size; i += 1) {
@@ -35,8 +35,10 @@ byte_string* pkcs7_pad_bytes(byte_string* x_bytes, size_t block_size) {
 }
 
 // unpad bytes
-byte_string* pkcs7_unpad_bytes(byte_string* bytes) {
+byte_string* pkcs7_unpad_bytes(byte_string* bytes, size_t block_size) {
   // read last element and remove as many bytes as the value of that element
+  if (bytes->size == block_size) return bytes_clone(bytes);
+
   size_t unpad = bytes->data[bytes->size - 1];
   size_t new_size = bytes->size - unpad;
 
@@ -49,30 +51,30 @@ byte_string* pkcs7_unpad_bytes(byte_string* bytes) {
   return new_bytes;
 }
 
-/*
-  NOTE(Tristan): block_size is probably not needed as the key is the same as the block_size?
-  p_bytes is padded inside of the aes_cbc_encrypt function
-*/
 byte_string* aes_ebc_encrypt(byte_string* p_bytes, byte_string* k_bytes, size_t block_size) {
   byte_string* c_bytes = bytes_init(p_bytes->size);
 
   AES_KEY k;
-  AES_set_encrypt_key(k_bytes->data, block_size * 8, &k);
+  AES_set_encrypt_key(k_bytes->data, 128, &k);
   AES_ecb_encrypt(p_bytes->data, c_bytes->data, &k, AES_ENCRYPT);
   c_bytes->size += block_size;
 
-  return c_bytes;
+  byte_string* padded_c_bytes = pkcs7_pad_bytes(c_bytes, block_size);
+  bytes_free(c_bytes);
+  return padded_c_bytes;
 }
 
 byte_string* aes_ebc_decrypt(byte_string* c_bytes, byte_string* k_bytes, size_t block_size) {
   byte_string* p_bytes = bytes_init(c_bytes->size);
 
   AES_KEY k;
-  AES_set_decrypt_key(k_bytes->data, block_size * 8, &k);
+  AES_set_decrypt_key(k_bytes->data, 128, &k);
   AES_ecb_encrypt(c_bytes->data, p_bytes->data, &k, AES_DECRYPT);
   p_bytes->size += block_size;
 
-  return p_bytes;
+  byte_string* unpadded_p_bytes = pkcs7_unpad_bytes(p_bytes, block_size);
+  bytes_free(p_bytes);
+  return unpadded_p_bytes;
 }
 
 byte_string* aes_cbc_encrypt(byte_string* p_bytes, byte_string* k_bytes, byte_string* iv_bytes, size_t block_size) {
@@ -99,32 +101,31 @@ byte_string* aes_cbc_encrypt(byte_string* p_bytes, byte_string* k_bytes, byte_st
 }
 
 byte_string* aes_cbc_decrypt(byte_string* c_bytes, byte_string* k_bytes, byte_string* iv_bytes, size_t block_size) {
-  assert(c_bytes != NULL && c_bytes->size > 0);
-
   byte_string* p_bytes = bytes_init(c_bytes->size);
-  assert(p_bytes->capacity > 0);
-
-  byte_string* prev_iv_bytes = iv_bytes;
+  byte_string* prev_iv_bytes = bytes_clone(iv_bytes);
 
   for (size_t i = 0; i < c_bytes->size; i += block_size) {
-    byte_string* tmp_c_bytes = bytes_init(block_size);
-    memcpy(tmp_c_bytes->data, c_bytes->data + i, block_size);
-    tmp_c_bytes->size = block_size;
+    //byte_string* tmp_c_bytes = bytes_init(block_size);
+    //memcpy(tmp_c_bytes->data, c_bytes->data + i, block_size);
+    //tmp_c_bytes->size = block_size;
+
+    byte_string* tmp_c_bytes = bytes_from(c_bytes->data + i, block_size);
 
     byte_string* ebc_block_decrypt = aes_ebc_decrypt(tmp_c_bytes, k_bytes, block_size);
     byte_string* xor_bytes = bytes_xor(prev_iv_bytes, ebc_block_decrypt);
 
     for (size_t j = 0; j < block_size; j += 1) bytes_append(p_bytes, xor_bytes->data[j]);
 
-    if (prev_iv_bytes != iv_bytes) bytes_free(prev_iv_bytes);
-    prev_iv_bytes = ebc_block_decrypt;
+    bytes_free(prev_iv_bytes);
+    prev_iv_bytes = bytes_clone(ebc_block_decrypt);
 
     bytes_free(xor_bytes);
     bytes_free(tmp_c_bytes);
   }
 
-  byte_string* unpadded_p_bytes = pkcs7_unpad_bytes(p_bytes);
+  byte_string* unpadded_p_bytes = pkcs7_unpad_bytes(p_bytes, block_size);
   bytes_free(p_bytes);
+  bytes_free(prev_iv_bytes);
   return unpadded_p_bytes;
 }
 
