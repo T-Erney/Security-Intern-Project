c96ee7893847b3484ccbe18d094315b886d8d13a
Tristan Erney <terney@live.esu.edu>
2022-02-03 21:33:48 -0500

    Completed: Set 3 Challenge 18

---
+++ 3-Block_Stream_Crypto/set3_challenge18.c
@@ -0,0 +1,19 @@
+#include "../headers/conversions.h"
+#include "../headers/aes.h"
+#include "../headers/xor.h"
+#include "../headers/memory_output.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char* input = "L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==";
+    byte_string* c_bytes = base64_to_bytes(input);
+    byte_string* k_bytes = string_to_bytes("YELLOW SUBMARINE");
+    size_t nonce = 0;
+    byte_string* p_bytes = aes_ctr_mode(c_bytes, k_bytes, nonce);
+
+    mem_output(p_bytes);
+
+    return 0;
+}
